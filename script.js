10fa7e430bf4b85bfcd3b45333ac87584d798231
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-03-30 15:48:23 +0000

    Got it to do fetch

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function handleSubmit(){
-    const xhttp = new XMLHttpRequest()
-    const data = {
-        "username": document.getElementById("username").value,
-        "password": document.getElementById("password").value
-    }
+
     
-    xhttp.onload  = function(){
-        console.log(this.responseText)
-    }
-    
-    xhttp.open("POST" , "https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", true)
-    xhttp.setRequestHeader("Content-type" , "application/json");
-    xhttp.send(JSON.stringify(data))
+function handleSubmit() {
+	// const xhttp = new XMLHttpRequest();
+	// const data = {
+	// 	"username": document.getElementById("username").value,
+	// 	"password": document.getElementById("password").value
+	// };
+	// xhttp.onload = function() {
+	// 	console.log(this.responseText);
+	// };
+	var url = 'https://example.com/profile';
+	// var data = { username: 'andrew' };
+
+	const data = {
+		"username": document.getElementById("username").value,
+		"password": document.getElementById("password").value
+	};
+	fetch("https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api", {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+
+		}).then(res => res.json())
+		.then(response => console.log('Success:', JSON.stringify(response)))
+		.catch(error => console.error('Error:', error));
+
+
 }
+
